1a91b4d7799d60aab6463ac94160936e32a06047
Dashbah <dsbakhankova@edu.hse.ru>
2021-10-16 19:46:22 +0300

    some message

---
+++ Module1/2021.10.01/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace MyProject
+{
+    class Program
+    {
+        static void Print(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(arr[i] + " ");
+        }
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            //удаленный
+            int a = rnd.Next(1, 101);
+            //Console.WriteLine(a);
+            //дважды
+            int b = rnd.Next(1, 101);
+            
+            int[] arr = new int[100];
+            for (int i = 0; i < 99; i++)
+            {
+                if (i == a)
+                    continue;
+                else arr[i] = i;
+            }
+            arr[99] = b;
+            //перемешиваем массив.
+            Array.Sort(arr, (a, b) =>
+            {
+                return rnd.Next(-1, 2);
+            });
+            Print(arr);
+
+            int[] arr1 = new int[100];
+            for (int i = 0; i < 99; i++)
+            {
+                if (arr[i] != 0)
+                    arr1[arr[i]]++;
+            }
+            Console.WriteLine();
+            for (int i = 0; i < 99; i++)
+            {
+                if (arr1[i] == 2)
+                    Console.WriteLine("result: " + i);
+            }
+        }
+    }
+}
