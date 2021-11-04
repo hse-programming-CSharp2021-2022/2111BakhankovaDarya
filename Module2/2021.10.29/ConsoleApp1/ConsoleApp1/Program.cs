736e8992d21ba328d885d0c8284ce54309bdc3c4
Dashbah <dsbakhankova@edu.hse.ru>
2021-11-04 09:45:25 +0300

    hello

---
+++ Module2/2021.10.29/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+//2021.10.29
+namespace ConsoleApp1
+{
+    class MyComplex
+    {
+        double re, im;
+        public MyComplex(double xre, double xim)
+        { re = xre; im = xim; }
+        public static MyComplex operator --(MyComplex mc)
+        {
+            return new MyComplex(mc.re - 1, mc.im - 1);
+        }
+        public double Mod() { return Math.Abs(re * re + im * im); }
+        static public bool operator true(MyComplex f)
+        {
+            if (f.Mod() > 1.0) return true;
+            return false;
+        }
+        static public bool operator false(MyComplex f)
+        {
+            if (f.Mod() <= 1.0) return true;
+            return false;
+        }
+        public static MyComplex operator +(MyComplex a, MyComplex b)
+        {
+            return new MyComplex(a.re + b.re, a.im + b.im);
+        }
+        public static MyComplex operator *(MyComplex a, MyComplex b)
+        {
+            return new MyComplex(a.re * b.im - a.im * b.re, a.im * b.re + a.re * b.im);
+        }
+        public static MyComplex operator -(MyComplex a, MyComplex b)
+        {
+            return new MyComplex(a.re - b.re, a.im - b.im);
+        }
+        public static MyComplex operator /(MyComplex a, MyComplex b)
+        {
+            return new MyComplex((a.re * b.re + a.im * b.im) / (b.im * b.im + b.re * b.re), (a.im * b.re - a.re * b.im) / (b.re * b.re + b.im * b.im));
+        }
+        public override string ToString()
+        {
+            return re + "+(" + im + ")i";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyComplex a = new MyComplex(1, 2);
+            MyComplex b = new MyComplex(3, 4);
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+        }
+    }
+}
---
+++ Module2/2021.10.29/Task5/Task5/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Task5
+{
+    public class ConsolePlate
+    {
+        char _plateChar;
+        ConsoleColor _plateColor = ConsoleColor.White;
+       /* ConsoleColor ForegroundColor = ConsoleColor.Red;*/
+        public ConsolePlate()
+        {
+            _plateChar = 'A';
+        }
+        public ConsolePlate(char plateChar, ConsoleColor plateColor)
+        {
+            if ((int)plateChar <= 90 && (int)plateChar >= 65)
+                this._plateChar = plateChar;
+            else this.PlateChar = 'A';
+            this._plateColor = plateColor;
+        }
+        public char PlateChar
+        {
+            set
+            {
+                if ((int)_plateChar <= 90 && (int)_plateChar >= 65)
+                    _plateChar = value;
+                else
+                    _plateChar = 'A';
+            }
+            get { return _plateChar; }
+        }
+
+        public ConsoleColor PlateColor
+        {
+            set { _plateColor = value; }
+            get { return _plateColor; }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            ConsolePlate cp = new ConsolePlate();
+            ConsolePlate[] somePlates =
+            {
+                new ConsolePlate('*', ConsoleColor.Red),
+                cp,
+                new ConsolePlate ((char)70, ConsoleColor.Green)
+            };
+            foreach (ConsolePlate conP1 in somePlates)
+            {
+                Console.ForegroundColor = conP1.PlateColor;
+                Console.Write(conP1.PlateChar);
+            }
+            Console.ForegroundColor = ConsoleColor.Magenta;
+        }
+    }
+}
