88c32cefff77cf1d1f0ff8c46820ca103b49b1cc
Dashbah <dsbakhankova@edu.hse.ru>
2021-11-05 12:25:46 +0300

    hello!

---
+++ Module1/2021.10.14(дз не было)/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"Data.txt";
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
